e00cc3c566183ae562f138333645faa3fa47f80e
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-06-07 19:31:36 +0300

    Created method search which compare array elements with element we search and return index of element if we find it or -1 if not

--- SearchApp.java
+++ SearchApp.java
@@ -2,4 +2,14 @@ package task1;
 
 public class SearchApp {
 
+    private int search(int[] array, int elementToFind){
+        for(int i = 0; i < array.length; i++){
+            if(elementToFind == array[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
