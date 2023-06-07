e6a3c1463bbcb1b1c444775cb0b3b9b5e2fb8304
rudiuknastya <93525328+rudiuknastya@users.noreply.github.com>
2023-06-07 20:04:16 +0300

    Added steps variable in search method for counting steps quntity to find element. Created work method for working with console, creating array and calling search method

--- SearchApp.java
+++ SearchApp.java
@@ -1,14 +1,40 @@
 package task1;
 
+import java.util.Scanner;
+
 public class SearchApp {
 
     private int search(int[] array, int elementToFind){
+        int steps = 0;
         for(int i = 0; i < array.length; i++){
+            steps++;
             if(elementToFind == array[i]){
+                System.out.println("Steps quantity to find element: " + steps);
                 return i;
             }
         }
+
         return -1;
+    }
+    public void work(){
+        System.out.println("Enter array size: ");
+        Scanner scanner = new Scanner(System.in);
+        String arraySize = scanner.nextLine();
+        int SIZE = Integer.parseInt(arraySize);
+        int[] array = new int[SIZE];
+        for(int element: array){
+            System.out.print(element+ " ");
+        }
+        System.out.println();
+        System.out.println("Enter element you wanna find: ");
+        String element = scanner.nextLine();
+        int elementToFind = Integer.parseInt(element);
+        int foundElement = search(array, elementToFind);
+        if(foundElement == -1){
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Index of found element");
+        }
     }
 
 
